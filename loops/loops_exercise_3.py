f60f9519abfdfd70dab002e65cf8bed21d619403
Grace Kim <130432110+haeunkim48@users.noreply.github.com>
2024-04-05 01:48:16 +0900

    update

---
+++ loops/loops_exercise_3.py
@@ -0,0 +1,32 @@
+# # 1. Look at the example, infer the rules, and take a picture of the stars.
+# # The first row is given N (1 ≤ N ≤ 100).
+# # From the first line to the 2×N-1th line, the stars are printed in order.
+
+# for i in range (1, 101):
+#     s = " "
+#     for j in range (2*i+1):
+#         s = s + "*"
+#     print(s)
+
+# #2. You have to say hello to N test cases to solve this problem.
+
+# # across N lines
+
+# # "Hello World, Judge i!"
+
+# # Make a program to print out, where i is the number of the line.
+# # (1 ≤ N ≤ 200) is given. 
+# # print Hello World, Judge i! per line.
+
+# for i in range (1, 101):
+#     print ("Hello World, Judge", i, "!")
+
+# 3. Write a program that, given the natural number N, outputs one line from N to one.
+# given The first row is given a natural number N less than or equal to 100,000.
+# Print from the first line to the Nth line in order.
+
+
+def print_number(n):
+    for i in range (n, 0, -1):
+        print(i)
+print_number(3)
